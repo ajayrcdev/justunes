a9a5b3925b132b77d58fa86762c352d3ff820f6e
Akeel Nazir <akeel.mir@klm.com>
2017-09-09 17:17:57 +0200

    finally working example of multer

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,14 +1,27 @@
 import { Router } from 'express'
 import * as multer from 'multer'
+import * as path from 'path'
 
 const router: Router = Router()
-const upload = multer({ dest: 'uploads' })
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, 'uploads')
+  },
+  filename: function(req, file, callback) {
+    console.log(file)
+    callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+// const upload = multer({ storage: storage })
+const upload = multer({ dest: 'uploads/' })
 
 router.get('/', (req, res) => {
   res.sendStatus(200)
 })
 
-router.post('/tune', upload.single('test'), (req, res, next) => {
+router.post('/tune', upload.single('tune'), (req, res) => {
 
   if (!req.file) {
     console.log("No file received")
